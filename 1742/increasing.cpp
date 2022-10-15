b1c542c6eeb31434b4ad4ae175d06e7c074eb62d
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2022-10-15 15:20:12 +0530

    Create increasing.cpp

---
+++ 1742/increasing.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+void solution(){
+	int num;
+	cin>>num;
+
+	vector <long long> arr(num);
+	for(int i=0; i<num; i++){
+		cin>>arr[i];
+	}
+
+	int flag = 0;
+	sort(arr.begin(), arr.end());
+
+	for(int i=0; i<num-1; i++){
+
+		if(arr[i] == arr[i+1]){
+			cout<<"No\n";
+			flag =1;
+			break;
+		}
+	}
+
+	if(flag==0){
+		cout<<"Yes\n";
+	}
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	long long t;
+	cin>>t;
+
+	while(t--){
+		solution();
+	}
+		
+	return 0;
+}
