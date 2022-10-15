6f4877a372424a4d7be66bd07b522951e3ca9480
Ayush Rawat <94822337+theayushr@users.noreply.github.com>
2022-10-15 14:58:52 +0530

    Create 1742-sum.cpp

---
+++ 1742-Sum/1742-sum.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+ 
+using namespace std;
+ 
+void solution(){
+	int a,b,c;
+	cin>>a>>b>>c;
+ 
+	
+	if(a== b+c){
+		cout<<"Yes\n";
+	}
+	else if(b== a+c){
+		cout<<"Yes\n";
+	}
+	else if(c== a+b){
+		cout<<"Yes\n";
+	}
+	else{
+		cout<<"No\n";
+	}
+}
+
+
+int main(){
+ 
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+ 
+	long long t;
+	cin>>t;
+ 
+	while(t--){
+		solution();
+	}
+		
+	return 0;
+}
